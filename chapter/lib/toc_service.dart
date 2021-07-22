d0d83589e2095c2e4a82d05ac1ad660cd650934a
The28AWG <the28awg@gmail.com>
2021-07-22 10:02:00 +0500

    update

--- chapter/lib/toc_service.dart
+++ chapter/lib/toc_service.dart
@@ -16,6 +16,7 @@ class TocService {
             ..get()
             ..path = {
               ':id': bookId,
-            })
+            }
+            ..attribute = {'type': List})
           .execute();
 }
